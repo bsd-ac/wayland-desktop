76ba13f47cd82d89ad745850ad73b90ad38cfdc4
Aisha Tammy <aisha@aisha.cc>
2020-09-29 16:15:36 -0400

    wf test

--- .travis/travis_ci.sh
+++ .travis/travis_ci.sh
@@ -4,10 +4,10 @@
 # - package tester - check if tests pass
 # - spinner - repoman full -d checker
 
-if [ -z "${REPOMAN+S}" && -z "${PKGCHECK+S}" ]; then
-	exec .travis/package_tester.sh
-elif [ -z "${REPOMAN+S}" ]; then
+if [ ! -z "${REPOMAN+S}" ]; then
   exec python portage-portage-${PORTAGE_VER}/repoman/bin/repoman full -dx
-else
+elif [ ! -z "${PKGCHECK+S}" ]; then
   exec pkgcheck scan
+else
+	exec .travis/package_tester.sh
 fi
