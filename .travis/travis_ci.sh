ad634ba97951348b8dba53d721837e031bf9f201
Aisha Tammy <aisha@aisha.cc>
2020-09-29 12:22:42 -0400

    bootstrap cache

--- .travis/travis_ci.sh
+++ .travis/travis_ci.sh
@@ -5,7 +5,7 @@
 # - spinner - repoman full -d checker
 
 if [ -z ${PACKAGE+S} ]; then
-  exec ../spinner.sh "python ../../portage-portage-${PORTAGE_VER}/repoman/bin/repoman full -d"
+  exec ../spinner.sh "python ../portage-portage-${PORTAGE_VER}/repoman/bin/repoman full -d"
 else
 	exec .travis/package_tester.sh
 fi
