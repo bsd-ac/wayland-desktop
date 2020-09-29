ff0644a062c8490751fc69f2d36c284348c78605
Aisha Tammy <aisha@aisha.cc>
2020-09-29 12:40:03 -0400

    cha cha

--- .travis/travis_ci.sh
+++ .travis/travis_ci.sh
@@ -4,7 +4,7 @@
 # - package tester - check if tests pass
 # - spinner - repoman full -d checker
 
-if [ -z ${PACKAGE+S} ]; then
+if [ "${REPOMAN}" -eq "1" ]; then
   exec ../spinner.sh "python ../portage-portage-${PORTAGE_VER}/repoman/bin/repoman full -d"
 else
 	exec .travis/package_tester.sh
